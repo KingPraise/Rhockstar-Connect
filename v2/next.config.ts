77be9ba26e3bcb7494dbe8c530bc932be9a4d288
King Praise <kingpraise15@gmail.com>
2026-07-14 14:21:49 +0100

    Configure localtunnel domains for Next.js

--- v2/next.config.ts
+++ v2/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  allowedDevOrigins: ['rhockstar-dev-999.loca.lt', 'localhost:3000'],
 };
 
 export default nextConfig;
